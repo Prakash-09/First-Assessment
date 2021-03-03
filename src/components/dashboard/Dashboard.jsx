111abdc3dd92ea6aa44179adcc7c3832c95baa17
Prakash-09 <prakash@digitaldots.ai>
2021-03-03 10:48:56 +0530

    Implemented dynamic routing and manage customDefaultPage component for all routes

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
+import { Row, Col } from 'react-bootstrap';
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div>
-                Hello dashboard
+                <Row className="m-0">
+                    <Col className="p-0">
+                        Hello dashboard
+                    </Col>
+                </Row>
             </div>
         );
     }
